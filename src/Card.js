10ae61d13972189f7719416c07ce9154a8b00207
RaccoonNW <87783832+RaccoonNW@users.noreply.github.com>
2022-09-22 11:01:59 -0600

    added stats to card

--- src/Card.js
+++ src/Card.js
@@ -1,18 +1,24 @@
 import './App.css';
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
 
 function Card({pokemon}) {
         
     let pokeName
+    let hp
+    let attack
+    let defense
+    let specialAttack
+    let specialDefense
+    let speed
 
     if(pokemon) {
         pokeName = capitalizeFirstLetter(pokemon.name)
-        let hp = pokemon.stats[0].base_stat;
-        let attack = pokemon.stats[1].base_stat;
-        let defense = pokemon.stats[2].base_stat;
-        let specialAttack = pokemon.stats[3].base_stat;
-        let specialDefense = pokemon.stats[4].base_stat;
-        let speed = pokemon.stats[5].base_stat;
+        hp = pokemon.stats[0].base_stat;
+        attack = pokemon.stats[1].base_stat;
+        defense = pokemon.stats[2].base_stat;
+        specialAttack = pokemon.stats[3].base_stat;
+        specialDefense = pokemon.stats[4].base_stat;
+        speed = pokemon.stats[5].base_stat;
     }
 
     function capitalizeFirstLetter(string) {
@@ -27,7 +33,7 @@ function Card({pokemon}) {
                 <div className={`card-name ${pokemon.types[0].type.name}` + 1}>
                     <div className='card-name-text'>
                         <h3>{pokeName}</h3>
-                        <h4>{pokemon.types[0].type.name}</h4>
+                        <h4>{hp + ' HP'}</h4>
                     </div>
                     <div className={`card-img ${pokemon.types[0].type.name}`}>
                         <img src={pokemon.sprites.front_default} alt='pokemon'/>
@@ -39,14 +45,39 @@ function Card({pokemon}) {
                 </div> */}
 
                 <div className={`card-ability ${pokemon.types[0].type.name}` + 1}>
-                    <p>
+                    <table>
+                        <tr>
+                            <th>Attack</th> 
+                            <th> <progress className='progress' id="health" value={attack} max="216"></progress></th>
+                        </tr>
+                        <tr>
+                            <th>Defense</th>
+                            <th><progress className='progress' id="defense" value={defense} max="250"></progress></th>
+                        </tr>
+                        <tr>
+                            <th>Speed</th>
+                            <th><progress className='progress' id="speed" value={speed} max="200"></progress></th>
+                        </tr>
+                    </table>
+                    
+                    {/* <p>
                         {pokemon.abilities[0].ability.name}
-                    </p>
+                    </p> */}
 
                 </div>
 
                 <div className={`card-attack ${pokemon.types[0].type.name}` + 1}>
-                    <p>{pokemon.moves[0].move.name}</p>
+                <table>
+                        <tr>
+                            <th>Special Attack</th>
+                            <th><progress className='progress' id="special-attack" value={specialAttack} max="194"></progress></th>
+                        </tr>
+                        <tr>
+                            <th>Special Defense</th>
+                            <th><progress className='progress' background="yellow" color='yellow' id="special-defense" value={specialDefense} max="250"></progress></th>
+                        </tr>
+                    </table>
+                    {/* <p>{pokemon.moves[0].move.name}</p> */}
                 </div>
             </div>
         )
